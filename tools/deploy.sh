396bb40b52432f2f019f3afd67ed3d818ab2c7ad
Justin Rice <jsrice7391@gmail.com>
2022-07-07 09:21:00 -0400

    Added the rest of the commands

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -1,5 +1,17 @@
 #! /bin/sh
 
+
+
+IMAGE_ID=$(docker images | grep "^$IMAGE_NAME" | awk '{print $3}')
+REPOSITORY_NAME="cloud-hippie-ecr-repository"
+ECR_URL="$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$REPOSITORY_NAME:$IMAGE_ID"
+
+
+if [ -z "$IMAGE_ID" ]; then
+    echo "Image $IMAGE_NAME not found"
+    exit 1
+fi
+
 # 1. Check if the AWS Cli is installed. You can read about the AWS cli here: https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html
 if command -v aws >/dev/null 2>&1; then
   echo "AWS CLI is installed."
@@ -8,15 +20,20 @@ else
   exit 1
 fi
 
-# 2. Install if it is not there
 
 # 3. Using the AWS Command line, publish your Docker container to ECR.
-aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
+docker build -t ecr-repo-test .   
+docker tag ecr-repo-test $URL
+docker push $URL
 
-# Build the image
-docker build -t ecr-repo-test .       
 
-# 4. Use that same command line to verify that your container is published.
-docker tag ecr-repo-test $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/cloud-hippie-ecr-repository:ecr-repo-test
+# 5. Get the image URI from ECR.
+OUTPUT=$(aws ecr describe-image --repository-name  --image-id $IMAGE_ID)
 
-docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/cloud-hippie-ecr-repository:ecr-repo-test
+
+if [ $OUTPUT ]; then
+  echo "Image $IMAGE_NAME succssfully verified in ECR"
+else
+  echo "Image $IMAGE_NAME not published to ECR"
+  exit 1
+fi
