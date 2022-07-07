184bdc604308c0158bb575c39edd798ddd07ab19
Justin Rice <jsrice7391@gmail.com>
2022-07-07 18:09:31 -0400

    Added the solution

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -1,11 +1,13 @@
 #! /bin/sh
 
 
-
-IMAGE_ID=$(docker images | grep "^$IMAGE_NAME" | awk '{print $3}')
+IMAGE_NAME=$1
+IMAGE_ID="$(docker images | grep "^$IMAGE_NAME" | awk '{print $3}')"
 REPOSITORY_NAME="cloud-hippie-ecr-repository"
-ECR_URL="$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$REPOSITORY_NAME:$IMAGE_ID"
+ECR_URL="$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$REPOSITORY_NAME:$IMAGE_NAME"
 
+echo "Logging in to AWS ECR..."
+aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
 
 if [ -z "$IMAGE_ID" ]; then
     echo "Image $IMAGE_NAME not found"
@@ -22,18 +24,18 @@ fi
 
 
 # 3. Using the AWS Command line, publish your Docker container to ECR.
-docker build -t ecr-repo-test .   
-docker tag ecr-repo-test $URL
-docker push $URL
+docker build -t $IMAGE_NAME .   
+docker tag $IMAGE_NAME $ECR_URL
+docker push $ECR_URL
 
 
-# 5. Get the image URI from ECR.
-OUTPUT=$(aws ecr describe-image --repository-name  --image-id $IMAGE_ID)
+# # 5. Get the image URI from ECR.
+# OUTPUT=$(aws ecr describe-images --repository-name --image-id $IMAGE_ID)
 
 
-if [ $OUTPUT ]; then
-  echo "Image $IMAGE_NAME succssfully verified in ECR"
-else
-  echo "Image $IMAGE_NAME not published to ECR"
-  exit 1
-fi
+# if [ $OUTPUT ]; then
+#   echo "Image $IMAGE_NAME succssfully verified in ECR"
+# else
+#   echo "Image $IMAGE_NAME not published to ECR"
+#   exit 1
+# fi
